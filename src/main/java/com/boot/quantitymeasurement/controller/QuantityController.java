e6c3d342f9a72330059bce13bbfe587cf1f71d76
bhavesh357 <bkadam357@gmail.com>
2020-07-07 23:04:29 +0530

    ADDED: swagger configuration

--- src/main/java/com/boot/quantitymeasurement/controller/QuantityController.java
+++ src/main/java/com/boot/quantitymeasurement/controller/QuantityController.java
@@ -6,6 +6,7 @@ import com.boot.quantitymeasurement.model.Quantity;
 import com.boot.quantitymeasurement.model.Response;
 import com.boot.quantitymeasurement.service.QuantityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,4 +36,6 @@ public class QuantityController {
     public Response handleQuantityException(QuantityException ex){
         return new Response(ex.getCode(),ex.getMessage(),null);
     }
+
+
 }
